6cd53e220724602353b9f2cf03e8434c079e0c6d
=raghav <raghu112220gmail.com>
2019-05-07 19:06:28 +0530

    adding ads static file

--- faculty/settings.py
+++ faculty/settings.py
@@ -122,11 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'app-root/repo/wsgi/static'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
